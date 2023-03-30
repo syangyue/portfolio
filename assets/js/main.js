2d74615f2811f78869d61abfada36a10466830e5
Yangyue Song <syangyue1990@gmail.com>
2023-03-30 15:45:38 -0700

    updated footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
